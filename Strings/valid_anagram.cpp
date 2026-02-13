a7ad3655f880da2498990857f7481df2f6af2c37
Aishwarya-B-Sanjay <b.sanjay.aishwarya@gmail.com>
2026-02-13 21:59:12 +0530

    Create valid_anagram.cpp

---
+++ Strings/valid_anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        map<char,int> m;
+
+        for(char c : s)
+            m[c]++;
+
+        for(char c : t)
+            m[c]--;
+
+        for(auto x : m)
+        {
+            if(x.second != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
